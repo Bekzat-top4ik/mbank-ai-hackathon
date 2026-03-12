e45c40c9bb56f3ceba5d01ca611cde93739d173e
Bekzat-top4ik <bekzatasanov2009@gmail.com>
2026-03-12 18:28:44 +0600

    Add Django project

---
+++ README.txt
@@ -0,0 +1,7 @@
+
+SETUP:
+
+1 Install python
+2 pip install -r requirements.txt
+3 python manage.py migrate
+4 python manage.py runserver
---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def dashboard(request):
+    context = {
+        'user_name': 'Bekzat',
+        'balance': 24500,
+        'income': 12000,
+        'expense': 5300,
+        'goal_progress': 68,
+        'transactions': [
+            {'title': 'Продукты', 'amount': '-850 сом', 'date': '12 мар'},
+            {'title': 'Такси', 'amount': '-230 сом', 'date': '11 мар'},
+            {'title': 'Пополнение', 'amount': '+5000 сом', 'date': '10 мар'},
+            {'title': 'Кофе', 'amount': '-180 сом', 'date': '10 мар'},
+        ]
+    }
+    return render(request, 'dashboard.html', context)
